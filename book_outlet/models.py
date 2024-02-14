27ca1fc088dbee0f9acc4444da41a8cc1c66d66c
Moghany <m.osama.elmoghany@gmail.com>
2024-02-14 11:04:20 +0200

    more to admin v2

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -7,6 +7,12 @@ from django.utils.text import slugify
 class Country(models.Model):
     name = models.CharField(max_length=50)
     code = models.CharField(max_length=2)
+    
+    def __str__(self):
+        return f"{self.name}"
+        
+    class Meta:
+        verbose_name_plural = "Countries"
 
 class Address(models.Model):
     street = models.CharField(max_length=50)
